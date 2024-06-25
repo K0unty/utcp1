3c9ec085453bba97eb4c89741e39015baa41caf3
K0unty <119057948+K0unty@users.noreply.github.com>
2024-06-25 07:56:00 +0000

    .

---
+++ panties/t1.py
@@ -0,0 +1,21 @@
+# Testing python files 
+
+# Printing in console 
+
+# Defining Colors
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+BLUE = "\033[1;34m"
+RESET = "\033[0m"
+
+print(f"""
+      {RED}
+      
+▀██▀─▄███▄─▀██─██▀██▀▀█
+─██─███─███─██─██─██▄█
+─██─▀██▄██▀─▀█▄█▀─██▀█
+▄██▄▄█▀▀▀─────▀──▄██▄▄█
+
+      {RESET}
+      """)
+
